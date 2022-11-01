8df0484cca80389da3f9a06a015a8259adcc0ee4
nekago <nekarulez@gmail.com>
2022-11-01 11:52:38 +0200

    header, footer, about us, partners, experience

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { LicensesComponent } from './components/licenses/licenses.component';
+import { PartnersComponent } from './components/partners/partners.component';
+import { ReviewsComponent } from './components/reviews/reviews.component';
+import { ServicesComponent } from './components/services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ExperienceComponent,
+    AboutUsComponent,
+    LicensesComponent,
+    PartnersComponent,
+    ReviewsComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule
